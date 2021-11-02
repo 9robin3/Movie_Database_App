af6ab4349be15cc52b03ff21a47e9b88bf585a38
9robin3 <andblom.robin@gmail.com>
2021-11-02 10:52:22 +0100

    ASP.NET MVC Solution added
    ASP.NET MVC Solution added

---
+++ Movie_Database_App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Movie_Database_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
